dbacc5cb4590f87e3e49d49ef2bc50260767eeec
juju <202444006@itc.ac.kr>
2025-03-24 18:32:32 +0900

    zip alternative

--- week03.py
+++ week03.py
@@ -1,7 +1,18 @@
-def inters(l1,l2):
-    s1 = set(l1)
-    s2 = set(l2)
-    return list(s1 & s2)
-l1=[45,5,22,31,7,19]
-l2=[22,1,5,2,7,28,27,19,13,41]
-print(inters(l1,l2))
+def myZip(l1, l2):
+    l12 = 0
+    if(len(l1) > len(l2)):
+        l12 = len(l2)
+    else:
+        l12 = len(l1)
+
+    l3 = list()
+    for i in range(l12):
+        l3.append((l1[i], l2[i]))
+    return l3
+
+group = ['BlackPink', 'Hot', 'Seventeen', 'NJZ']
+ratings = [1,2]
+#ratings = [1,2,4,3,100]
+
+group_rating = myZip(group,ratings)
+print(group_rating)
