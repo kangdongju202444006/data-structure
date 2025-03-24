b8814b11dd93f6e6dba52adbf4d678705a644e21
juju <202444006@itc.ac.kr>
2025-03-24 17:58:39 +0900

    set2 add

--- week03.py
+++ week03.py
@@ -1,5 +1,22 @@
-cities = ['Incheon','seoul','Incheon','Incheon','Gwangju']
-cities = set(cities)
-cities.add('Incheon')
-cities.add('Suwon')
-print(cities) #dic과 set은 순서가 없다.
+def duplicate_city(cities):
+    result_city = []
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result_city.append(city)
+    return result_city
+
+
+cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
+# cities = set(cities)
+# cities.add('Incheon')
+# cities.add('Suwon')
+cities.append('Incheon')
+cities.append('Seoul')
+cities.append('Suwon')
+print(cities)
+print(set(duplicate_city(cities)))
