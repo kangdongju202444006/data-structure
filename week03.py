f425935b6cb6e90249abfd74c98c56cfc990746c
juju <202444006@itc.ac.kr>
2025-03-24 17:38:51 +0900

    zip

--- week03.py
+++ week03.py
@@ -1,17 +1,5 @@
-def move_zeros(l):
-    zero_idx = 0
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if zero_idx != i:
-                l[i] = 0
-            zero_idx = zero_idx + 1
-    return l
+group = ['HOT', 'Seventeen','Black Pink','NJZ']
+ratings = [1,2,4,3]
 
-l = [99, 0, -7, 0, 16]
-move_zeros(l)
-
-
-
-print(l)
+group_rating = list(zip(group, ratings))
+print(group_rating)
