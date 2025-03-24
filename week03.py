1084662b59298df6c20484ea9ce78ea1de944249
juju <202444006@itc.ac.kr>
2025-03-24 17:34:29 +0900

    array.array

--- week03.py
+++ week03.py
@@ -1,3 +1,17 @@
-l =[99, 0, -7, 0 ,16]
-for i in range(len(l)):
-    print(f"{l[i]:3}, {id(l[i])}")
+def move_zeros(l):
+    zero_idx = 0
+    for i in range(len(l)):
+        n = l[i]
+        if n != 0:
+            l[zero_idx] = n
+            if zero_idx != i:
+                l[i] = 0
+            zero_idx = zero_idx + 1
+    return l
+
+l = [99, 0, -7, 0, 16]
+move_zeros(l)
+
+
+
+print(l)
