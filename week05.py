84d1ca61962a223a7a3a5eeb132cfe2fa92dd1a9
juju <202444006@itc.ac.kr>
2025-04-07 17:45:39 +0900

    week05 v0.3 peek

--- week05.py
+++ week05.py
@@ -25,10 +25,14 @@ class Stack:
         popped_node.link = None
         return popped_node.data
 
+    def peek(self):
+        return self.top.data
+
 
 s1 = Stack()
 print(s1.pop())
 s1.push("Data structure")
 s1.push("Database")
-print(s1.pop())
-print(s1.pop())
+print(s1.peek())
+#print(s1.pop())
+#print(s1.pop())
